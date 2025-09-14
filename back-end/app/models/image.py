80e6bef68c1d365e908a3bd5dd3c004fefd0667c
PauloS5 <paulosocratesdesouza@gmail.com>
2025-09-14 18:01:57 -0500

    model de image criado

--- back-end/app/models/image.py
+++ back-end/app/models/image.py
@@ -1,1 +1,10 @@
-﻿
+﻿from sqlmodel import SQLModel, Field
+
+class Image(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True,
+        title="ID", description="Identificador único da imagem")
+    filename: str = Field(unique=True, regex=r"^uploads/[a-zA-z\d_]+$",
+        title="Nome do arquivo", description="Diretório onde o arquivo de imagem está salvo")
+    title: str = Field(title="Título", description="Título da imagem para aparecer na interface")
+    desc:str = Field(default=None,
+        title="Descrição", description="Descrição da imagem")

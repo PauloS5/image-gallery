afdfc8732dde2467643a9101674f9952e8f77d87
PauloS5 <paulosocratesdesouza@gmail.com>
2025-09-14 18:21:48 -0500

    schemas de image criados - #6

--- back-end/app/schemas/image.py
+++ back-end/app/schemas/image.py
@@ -1,1 +1,30 @@
-﻿
+﻿from sqlmodel import SQLModel, Field
+
+class ImageBase(SQLModel, table=True):
+    title: str = Field(
+        title="Título",
+        description="Título da imagem para aparecer na interface")
+    desc: str = Field(
+        default=None,
+        title="Descrição",
+        description="Descrição da imagem")
+    
+class ImageCreate(ImageBase):
+    pass
+
+class ImageRead(ImageBase):
+    id: int = Field(
+        title="ID",
+        description="Identificador único da imagem")
+    content_type: str = Field(regex="^image/[a-z]+$")
+    url: str
+
+class ImageUpdate(ImageBase):
+    id: int = Field(
+        title="ID",
+        description="Identificador único da imagem")
+
+class ImageDelete(ImageBase):
+    id: int = Field(
+        title="ID",
+        description="Identificador único da imagem")

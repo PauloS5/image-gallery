6213bc4d559c5ba77fc4698c7d18fadb0d028d97
PauloS5 <paulosocratesdesouza@gmail.com>
2025-09-14 19:17:20 -0500

    arquivo com configurações do banco de dados criado - #9

---
+++ back-end/app/core/database.py
@@ -0,0 +1,21 @@
+﻿from sqlmodel import SQLModel, Session, create_engine
+
+DB_USER = "root"
+DB_PASSWORD = ""
+DB_HOST = "localhost"
+DB_PORT = "3306"
+DB_NAME = "dbImageGallery"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session(session):
+    with Session(engine) as session:
+        yield session
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":  
+    create_db_and_tables()
